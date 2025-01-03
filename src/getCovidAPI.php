f0603ca426019b4517f8ff20e6cc97bb91333260
Pedro Henrique <pedrohenr1qqstudios@gmail.com>
2025-01-02 23:45:26 -0300

    Feat: add API connection

---
+++ src/getCovidAPI.php
@@ -0,0 +1,12 @@
+<?php
+  function getDatasFromCountry($country){
+    $api_key = $_ENV['API_KEY'];
+    $url = $api_key . $country;
+    $json=  (file_get_contents($url));
+  
+    $datas = json_decode($json, true);
+
+    return $datas;
+  }
+
+?>
